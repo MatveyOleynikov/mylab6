c6574719551320c2e062cdabeebfd763c67589c5
MatveyOleynikov <ochoba-1@mail.ru>
2023-04-14 20:50:49 +0300

    print vector

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+void printVector(const vector<int>& vec){
+    for (auto elem: vec){
+        cout << elem << " ";
+    }
+    cout << "\n";
+}
 
 int main(){
 
