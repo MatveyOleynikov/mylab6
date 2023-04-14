9136806141be458afe59826658506ccc84bc0c5b
MatveyOleynikov <ochoba-1@mail.ru>
2023-04-14 21:01:59 +0300

    dbg

--- main.cpp
+++ main.cpp
@@ -3,6 +3,12 @@
 
 using namespace std;
 
+#ifdef MATVEY
+#define dbg(x) do { cout << __LINE__ << ": " << #x << " = " << x << endl;} while (0)
+#else
+#define dbg(x)
+#endif
+
 void printVector(const vector<int>& vec){
     for (auto elem: vec){
         cout << elem << " ";
@@ -25,5 +31,8 @@ int main(){
 
     printVectorVector(vct);
 
+    int a = 5;
+
+    dbg(a);
     return 0;
 }
